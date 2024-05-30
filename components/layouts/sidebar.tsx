564ecdf3a268f385a5bcf051cc9a70415e81d779
JagadishBujji <jagadish00198@gmail.com>
2024-05-30 12:34:57 +0530

    sidebar

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -91,7 +91,7 @@ const Sidebar = () => {
                     <div className="flex items-center justify-between px-4 py-3">
                         <Link href="/" className="main-logo flex shrink-0 items-center">
                             <img className="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="logo" />
-                            <span className="align-middle text-2xl font-semibold dark:text-white-light lg:inline ltr:ml-1.5 rtl:mr-1.5">VRISTO</span>
+                            <span className="align-middle text-2xl font-semibold dark:text-white-light lg:inline ltr:ml-1.5 rtl:mr-1.5">FourTake</span>
                         </Link>
 
                         <button
