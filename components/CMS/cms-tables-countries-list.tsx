55282a3f3443efdba2f62279722c7abf3b4381b9
JagadishBujji <jagadish00198@gmail.com>
2024-06-06 10:56:13 +0530

    export to excell added in country table

--- components/CMS/cms-tables-countries-list.tsx
+++ components/CMS/cms-tables-countries-list.tsx
@@ -9,8 +9,7 @@ import 'tippy.js/dist/tippy.css';
 import ComponentsModalOptionalSizes from '../Reusable/Modal/components-modal-optional-sizes';
 import IconMenuDocumentation from '../icon/menu/icon-menu-documentation';
 
-const rowData =
- [
+const rowData = [
     {
         id: 1,
         firstName: 'Caroline',
@@ -182,7 +181,7 @@ const CmsTablesCountriesList = ({ title }: CmsTablesCountriesListProps) => {
                         paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
                     />
                 </div>
-                <div className="flex items-center gap-4">
+                {/* <div className="flex items-center gap-4">
                     <button
                         disabled
                         className="flex select-none items-center gap-2 rounded-lg px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
@@ -234,7 +233,7 @@ const CmsTablesCountriesList = ({ title }: CmsTablesCountriesListProps) => {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
                         </svg>
                     </button>
-                </div>
+                </div> */}
             </div>
         </>
     );
