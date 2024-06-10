73b5db24158ae9fdf9b64f4c7862f6dfc5dd8800
Rajeswari21300 <rajeswari21300@gmail.com>
2024-06-10 13:17:11 +0530

    onchange add form, edit form, delete form

--- components/layouts/table-layout.tsx
+++ components/layouts/table-layout.tsx
@@ -18,19 +18,30 @@ interface TableLayoutProps {
     totalPages: number;
     tableColumns: object[];
     ActionModal: any;
-    handleSave?: any;
+    handleSumbit?: any;
     exportColumns?: string[];
+    handleDelete:any;
 }
 
-const TableLayout: React.FC<TableLayoutProps> = ({ title, data, totalPages, tableColumns, ActionModal, exportColumns }) => {
+const TableLayout: React.FC<TableLayoutProps> = ({ title, data, totalPages,handleDelete, handleSumbit, tableColumns, ActionModal, exportColumns }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [editData, setEditData] = useState(null);
     const [search, setSearch] = useState('');
+    const [addData, setAddData] = useState({});
+
+    console.log('data', data);
 
     const handleEdit = (object: any) => {
         console.log('object: ', object);
         setIsOpen(true);
-        setEditData(object);
+        setAddData(object);
+    };
+
+    const handleInputChange = (e: any) => {
+        const { value, id } = e.target;
+       
+        setAddData({ ...addData, [id]: value });
+        console.log('addData', addData);
     };
 
     const handleExport = () => {
@@ -40,7 +51,10 @@ const TableLayout: React.FC<TableLayoutProps> = ({ title, data, totalPages, tabl
     };
 
     const handleSave = () => {
-        console.log('HandleSave');
+        if (handleSumbit(addData)) {
+            setIsOpen(false);
+            setAddData({});
+        }
     };
 
     return (
@@ -71,166 +85,11 @@ const TableLayout: React.FC<TableLayoutProps> = ({ title, data, totalPages, tabl
             </div>
             <div className="panel mt-5 overflow-hidden border-0 p-0">
                 <div className="table-responsive">
-                    <PaginationTable data={data} tableColumns={tableColumns} handleEdit={handleEdit} />
-                    {/* <Table data={data} /> */}
-                    {/* <div className="pagination">
-                        {[...Array(totalPages)].map((_, index) => (
-                            <button key={index}>{index + 1}</button>
-                        ))}
-                    </div> */}
+                    <PaginationTable data={data} tableColumns={tableColumns} handleDelete={handleDelete} handleEdit={handleEdit} />
                 </div>
             </div>
 
-            {/* {actionModal === 'countryActionModel' ? (
-                <CountryActionModal isOpen={isOpen} setIsOpen={setIsOpen} handleSave={handleSave} />
-            ) : (
-                actionModal === 'visatype' && <VisaTypesActionModal isOpen={isOpen} setIsOpen={setIsOpen} handleSave={handleSave} />
-            )} */}
-
-            {/* {actionModal && actionModal(isOpen, setIsOpen, handleSave)} */}
-            <ActionModal isOpen={isOpen} setIsOpen={setIsOpen} handleSave={handleSave} />
-
-            {/* <VisaTypesActionModal isOpen={isOpen} setIsOpen={setIsOpen} handleSave={handleSave} /> */}
-
-            {/* {actionModal && <actionModal isOpen={isOpen} setIsOpen={setIsOpen} handleSave={handleSave}/>} */}
-
-            {/* <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" open={isOpen} onClose={() => setIsOpen(false)}>
-                    <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-                        <div className="fixed inset-0 " />
-                    </Transition.Child>
-                    <div className="fixed inset-0 z-[999] overflow-y-auto bg-[black]/60">
-                        <div className="flex min-h-screen items-start justify-center px-4">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="panel my-8 w-full max-w-5xl overflow-hidden rounded-lg border-0 p-0 text-black dark:text-white-dark">
-                                    <div className="flex items-center justify-between bg-[#fbfbfb] px-5 py-3 dark:bg-[#121c2c]">
-                                        <h5 className="text-lg font-bold">Add Country</h5>
-                                        <button onClick={() => setIsOpen(false)} type="button" className="text-white-dark hover:text-dark">
-                                            <IconX />
-                                        </button>
-                                    </div>
-
-                                    <div className="p-5">
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="countrysname">Country Name</label>
-                                                <input id="countrysname" type="text" placeholder="Enter Country Name" className="form-input" />
-                                            </div>
-                                            <div className="mb-5">
-                                                <label htmlFor="languages">Languages</label>
-                                                <input id="languages" type="text" placeholder="Enter Languages" className="form-input" />
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="dailingcode">Dailing Code</label>
-                                                <input id="dailingcode" type="tel" placeholder="Enter dailing Code" className="form-input" />
-                                            </div>
-                                            <div className="mb-5">
-                                                <label htmlFor="capital">Capital</label>
-                                                <input id="capital" type="text" placeholder="Enter Capital" className="form-input" />
-                                            </div>
-                                        </div>
-                                        <div className="mb-2 grid grid-cols-1 gap-5 md:grid-cols-1 ">
-                                            <div className="dropdown">
-                                                <label htmlFor="role">Cities</label>
-                                                <select className="form-input" defaultValue="" id="role">
-                                                    <option value="" disabled={true}>
-                                                        Cities
-                                                    </option>
-                                                    <option value="Chennai">Chennai</option>
-                                                    <option value="Vellore">Vellore</option>
-                                                    <option value="Bengaluru">Bengaluru</option>
-                                                    <option value="New Delhi">New Delhi</option>
-                                                    <option value="Mangalore">Mangalore</option>
-                                                    <option value="Mumbai">Mumbai</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="countrydetails">Country Details</label>
-                                                <textarea id="countrydetails" rows={3} placeholder="Enter Country Details" className="form-textarea min-h-[80px] resize-none"></textarea>
-                                            </div>
-                                            <div className="mb-5">
-                                                <label htmlFor="climate">Climate</label>
-                                                <textarea id="climate" rows={3} placeholder="Enter Climate" className="form-textarea min-h-[80px] resize-none"></textarea>
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="currency">Currency</label>
-                                                <input id="currency" type="tel" placeholder="Enter Currency" className="form-input" />
-                                            </div>
-                                            <div className="mb-5">
-                                                <label htmlFor="timezone">Time Zone</label>
-                                                <input id="timezone" type="text" placeholder="Enter Time Zone" className="form-input" />
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-1 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="additionalinfo">Additional Info</label>
-                                                <textarea id="additionalinfo" rows={3} placeholder="Enter Additional Info" className="form-textarea min-h-[80px] resize-none"></textarea>
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-1 ">
-                                            <div className="mb-5">
-                                                <label htmlFor="website">Website</label>
-                                                <input id="website" type="text" placeholder="Enter Website" className="form-input" />
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-3 ">
-                                            <div className="mb-5">
-                                                <label className="flex cursor-pointer items-center">
-                                                    <input type="checkbox" className="form-checkbox bg-white dark:bg-black" />
-                                                    <span className="text-white-dark">Is popular Country</span>
-                                                </label>
-                                            </div>
-                                            <div className="mb-5">
-                                                <label className="flex cursor-pointer items-center">
-                                                    <input type="checkbox" className="form-checkbox bg-white dark:bg-black" />
-                                                    <span className="text-white-dark">Enable outsource application center</span>
-                                                </label>
-                                            </div>
-                                            <div className="mb-5">
-                                                <label className="flex cursor-pointer items-center">
-                                                    <input type="checkbox" className="form-checkbox bg-white dark:bg-black" />
-                                                    <span className="text-white-dark">Enable Jurisdiction</span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-cols-1 gap-5 md:grid-cols-3 ">
-                                            <div className="mb-5">
-                                                <ComponentsFormsFileUploadSingle />
-                                            </div>
-                                            <div className="mb-5">
-                                                <ComponentsFormsFileUploadMulti />
-                                            </div>
-                                        </div>
-                                        <div className="mt-8 flex items-center justify-end">
-                                            <button onClick={() => setIsOpen(false)} type="button" className="btn btn-outline-danger">
-                                                Cancel
-                                            </button>
-                                            <button onClick={handleSave} type="button" className="btn btn-primary ltr:ml-4 rtl:mr-4">
-                                                Save
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition> */}
+            <ActionModal isOpen={isOpen} setAddData={setAddData} handleInputChange={handleInputChange} setIsOpen={setIsOpen} handleSave={handleSave} addData={addData} />
         </>
     );
 };
