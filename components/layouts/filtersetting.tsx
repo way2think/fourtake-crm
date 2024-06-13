50e65391d37205401c0f5a06f98171ada6358660
sandy-d <19mca018santhosh.d@gmail.com>
2024-06-13 18:32:02 +0530

    Pull

--- components/layouts/filtersetting.tsx
+++ components/layouts/filtersetting.tsx
@@ -1,4 +1,5 @@
 'use client';
+import IconSearch from '../icon/icon-search';
 
 interface FiltersettingProps {
     showCustomizer: any;
@@ -11,9 +12,8 @@ const Filtersetting: React.FC<FiltersettingProps> = ({ showCustomizer, setShowCu
             <div className={`${(showCustomizer && '!block') || ''} fixed inset-0 z-[51] hidden bg-[black]/60 px-4 transition-[display]`} onClick={() => setShowCustomizer(false)}></div>
 
             <nav
-                className={`${
-                    (showCustomizer && 'ltr:!right-0 rtl:!left-0') || ''
-                } fixed bottom-0 top-0 z-[51] w-full max-w-[400px] bg-white p-4 shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-[right] duration-300 dark:bg-black ltr:-right-[400px] rtl:-left-[400px]`}
+                className={`${(showCustomizer && 'ltr:!right-0 rtl:!left-0') || ''
+                    } fixed bottom-0 top-0 z-[51] w-full max-w-[400px] bg-white p-4 shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-[right] duration-300 dark:bg-black ltr:-right-[400px] rtl:-left-[400px]`}
             >
                 {/* <button
                     type="button"
@@ -24,7 +24,74 @@ const Filtersetting: React.FC<FiltersettingProps> = ({ showCustomizer, setShowCu
                 </button> */}
 
                 <div className="perfect-scrollbar h-full overflow-y-auto overflow-x-hidden">
-                    <div className="flex h-screen items-center justify-center">Filter</div>
+                    <div className="grid grid-cols-1 gap-5 m-5">
+
+                        <div className="relative">
+                            <input type="text" placeholder="Search" className="peer form-input py-2 ltr:pr-11 rtl:pl-11" />
+                            <button type="button" className="absolute top-1/2 -translate-y-1/2 peer-focus:text-primary ltr:right-[11px] rtl:left-[11px]">
+                                <IconSearch className="mx-auto" />
+                            </button>
+                        </div>
+
+
+                        <div className="dropdown ">
+                            <label htmlFor="embassy/vfs">Follow Up</label>
+                            <select className="form-input" defaultValue="" id="embassy">
+                                <option value="" >
+                                    [-Select-]
+                                </option>
+                                <option value="all">All</option>
+                                <option value="vfs">VFS</option>
+                                <option value="all">All</option>
+                            </select>
+                        </div>
+                        <div className="dropdown">
+                            <label htmlFor="embassy/vfs">Country</label>
+                            <select className="form-input" defaultValue="" id="embassy">
+                                <option value="" >
+                                    [-Select-]
+                                </option>
+                                <option value="andorra">Andorra</option>
+                                <option value="albania">Albania</option>
+                                <option value="bangladesh">Bangladesh</option>
+                            </select>
+                        </div>
+                        <div className="dropdown">
+                            <label htmlFor="embassy/vfs">Status</label>
+                            <select className="form-input" defaultValue="" id="embassy">
+                                <option value="" >
+                                    [-Select-]
+                                </option>
+                                <option value="embassy">Embassy</option>
+                                <option value="vfs">VFS</option>
+                                <option value="all">All</option>
+                            </select>
+                        </div>
+                        <div className="dropdown">
+                            <label htmlFor="embassy/vfs">Filter Lead statu</label>
+                            <select className="form-input" defaultValue="" id="embassy">
+                                <option value="" >
+                                    [-Select-]
+                                </option>
+                                <option value="hot">Hot</option>
+                                <option value="cold">Cold</option>
+                                <option value="warm">Warm</option>
+                            </select>
+                        </div>
+                        <div className="dropdown">
+                            <label htmlFor="embassy/vfs">Filter by Priority</label>
+                            <select className="form-input" defaultValue="" id="embassy">
+                                <option value="" >
+                                    [-Select-]
+                                </option>
+                                <option value="hot">Hot</option>
+                                <option value="cold">Cold</option>
+                                <option value="warm">Warm</option>
+                            </select>
+                        </div>
+
+
+                    </div>
                 </div>
             </nav>
         </div>
