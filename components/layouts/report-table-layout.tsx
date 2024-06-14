88346398e5b14bd8682170a6d98f4971cf38a8a3
JagadishBujji <jagadish00198@gmail.com>
2024-06-14 17:19:14 +0530

    lead range

--- components/layouts/report-table-layout.tsx
+++ components/layouts/report-table-layout.tsx
@@ -13,6 +13,7 @@ import IconX from '../icon/icon-x';
 import CountryActionModal from '../CMS/countries/CountryActionModal';
 import IconFile from '../icon/icon-zip-file';
 import ComponentsFormDatePickerTime from '../lead-management/lead-manage/components-form-date-picker-time';
+import ComponentsFormDatePickerRange from '../lead-management/lead-manage/components-form-date-picker-range';
 
 interface ReportTableLayoutProps {
     title: string;
@@ -73,29 +74,57 @@ const ReportTableLayout: React.FC<ReportTableLayoutProps> = ({ title, data, tota
         <>
             <div className="flex flex-wrap items-center justify-between gap-4">
                 <h2 className="text-xl">{title}</h2>
-                <div className="flex w-full  flex-col gap-4 sm:w-auto sm:flex-row sm:items-center sm:gap-3">
-                    {/* <div className="flex gap-3">
-                        {title !== 'Country Visa Types' && (
-                            <div>
-                                <button type="button" className="btn btn-primary" onClick={() => setIsOpen(true)}>
-                                    <IconUserPlus className="ltr:mr-2 rtl:ml-2" />
-                                    Add {title}
-                                </button>
-                            </div>
-                        )}
+                <div className="flex w-full  flex-col gap-5 sm:w-auto sm:flex-row sm:items-center sm:gap-4">
+                    <div className="flex gap-3">
+                        {/* {title !== 'Country Visa Types' && ( */}
                         <div>
+                            <div className="dropdown">
+                                <label htmlFor="leadStatus">Select User</label>
+                                <select className="form-input">
+                                    <option value="" disabled>
+                                        {' '}
+                                        Select User
+                                    </option>
+                                    <option value="hot">Kavitha N[Admin]</option>
+                                    <option value="cold">Cold</option>
+                                    <option value="warm">Warm</option>
+                                </select>
+                            </div>
+                        </div>
+                        {/* )} */}
+                        <div className="dropdown">
+                            <label htmlFor="leadStatus">Select Center </label>
+                            <select className="form-input">
+                                <option value="" disabled>
+                                    Select Center
+                                </option>
+                                <option value="hot">Hot</option>
+                                <option value="cold">Cold</option>
+                                <option value="warm">Warm</option>
+                            </select>
+                        </div>
+                        <div className="flex items-end justify-end">
+                            <ComponentsFormDatePickerRange />
+                        </div>
+
+                        <div className="flex items-center justify-center">
+                            <button type="button" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center">
                             <button type="button" className="btn btn-outline-primary" onClick={handleExport}>
                                 Export to Excel
                             </button>
                         </div>
                     </div>
-                    <div className="relative">
+                    {/* <div className="relative">
                         <input type="text" placeholder={`Search ${title}`} className="peer form-input py-2 ltr:pr-11 rtl:pl-11" value={search} onChange={(e) => setSearch(e.target.value)} />
                         <button type="button" className="absolute top-1/2 -translate-y-1/2 peer-focus:text-primary ltr:right-[11px] rtl:left-[11px]">
                             <IconSearch className="mx-auto" />
                         </button>
-                    </div>
-                    {title === 'Lead List' && (
+                    </div> */}
+                    {/* {title === 'Lead List' && (
                         <div>
                             <button type="button" className="btn btn-primary" onClick={handleFilter}>
                                 <IconFile className="ltr:mr-2 rtl:ml-2" />
@@ -105,66 +134,14 @@ const ReportTableLayout: React.FC<ReportTableLayoutProps> = ({ title, data, tota
                     )} */}
                 </div>
             </div>
-            <div className="mt-5">
-                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                    <div className="mb-5">
-                        <div className="dropdown">
-                            <label htmlFor="leadStatus">Select User</label>
-                            <select
-                                className="form-input"
-                            >
-                                <option value="">[-Select-]</option>
-                                <option value="hot">Hot</option>
-                                <option value="cold">Cold</option>
-                                <option value="warm">Warm</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <div className="dropdown">
-                            <label htmlFor="leadStatus">Select Center </label>
-                            <select
-                                className="form-input"
-                            >
-                                <option value="">[-Select-]</option>
-                                <option value="hot">Hot</option>
-                                <option value="cold">Cold</option>
-                                <option value="warm">Warm</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
-                    <div className="mb-5">
-                        <div className="dropdown">
-                            <label htmlFor="leadStatus">Select User</label>
-                            <select
-                                className="form-input"
-                            >
-                                <option value="">[-Select-]</option>
-                                <option value="hot">Hot</option>
-                                <option value="cold">Cold</option>
-                                <option value="warm">Warm</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <div className="">
-                            <ComponentsFormDatePickerTime />
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             <div className="panel mt-5 overflow-hidden border-0 p-0">
-                <div className="table-responsive">
-                    {/* <PaginationTable data={filterItem} tableColumns={tableColumns} handleDelete={handleDelete} handleEdit={handleEdit} /> */}
-                </div>
+                <div className="table-responsive">{/* <PaginationTable data={filterItem} tableColumns={tableColumns} handleDelete={handleDelete} handleEdit={handleEdit} /> */}</div>
             </div>
 
             {title == 'Lead List' && <Filtersetting showCustomizer={showCustomizer} setShowCustomizer={setShowCustomizer} />}
-
         </>
     );
 };
 
-export default ReportTableLayout; 
+export default ReportTableLayout;
