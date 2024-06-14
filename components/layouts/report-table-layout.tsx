93c6db52d9f881172cefde923f2ea760b0a863d6
JagadishBujji <jagadish00198@gmail.com>
2024-06-14 17:39:39 +0530

    Report daily

--- components/layouts/report-table-layout.tsx
+++ components/layouts/report-table-layout.tsx
@@ -74,57 +74,29 @@ const ReportTableLayout: React.FC<ReportTableLayoutProps> = ({ title, data, tota
         <>
             <div className="flex flex-wrap items-center justify-between gap-4">
                 <h2 className="text-xl">{title}</h2>
-                <div className="flex w-full  flex-col gap-5 sm:w-auto sm:flex-row sm:items-center sm:gap-4">
-                    <div className="flex gap-3">
-                        {/* {title !== 'Country Visa Types' && ( */}
-                        <div>
-                            <div className="dropdown">
-                                <label htmlFor="leadStatus">Select User</label>
-                                <select className="form-input">
-                                    <option value="" disabled>
-                                        {' '}
-                                        Select User
-                                    </option>
-                                    <option value="hot">Kavitha N[Admin]</option>
-                                    <option value="cold">Cold</option>
-                                    <option value="warm">Warm</option>
-                                </select>
+                <div className="flex w-full  flex-col gap-4 sm:w-auto sm:flex-row sm:items-center sm:gap-3">
+                    {/* <div className="flex gap-3">
+                        {title !== 'Country Visa Types' && (
+                            <div>
+                                <button type="button" className="btn btn-primary" onClick={() => setIsOpen(true)}>
+                                    <IconUserPlus className="ltr:mr-2 rtl:ml-2" />
+                                    Add {title}
+                                </button>
                             </div>
-                        </div>
-                        {/* )} */}
-                        <div className="dropdown">
-                            <label htmlFor="leadStatus">Select Center </label>
-                            <select className="form-input">
-                                <option value="" disabled>
-                                    Select Center
-                                </option>
-                                <option value="hot">Hot</option>
-                                <option value="cold">Cold</option>
-                                <option value="warm">Warm</option>
-                            </select>
-                        </div>
-                        <div className="flex items-end justify-end">
-                            <ComponentsFormDatePickerRange />
-                        </div>
-
-                        <div className="flex items-center justify-center">
-                            <button type="button" className="btn btn-primary">
-                                Submit
-                            </button>
-                        </div>
-                        <div className="flex items-center justify-center">
+                        )}
+                        <div>
                             <button type="button" className="btn btn-outline-primary" onClick={handleExport}>
                                 Export to Excel
                             </button>
                         </div>
                     </div>
-                    {/* <div className="relative">
+                    <div className="relative">
                         <input type="text" placeholder={`Search ${title}`} className="peer form-input py-2 ltr:pr-11 rtl:pl-11" value={search} onChange={(e) => setSearch(e.target.value)} />
                         <button type="button" className="absolute top-1/2 -translate-y-1/2 peer-focus:text-primary ltr:right-[11px] rtl:left-[11px]">
                             <IconSearch className="mx-auto" />
                         </button>
-                    </div> */}
-                    {/* {title === 'Lead List' && (
+                    </div>
+                    {title === 'Lead List' && (
                         <div>
                             <button type="button" className="btn btn-primary" onClick={handleFilter}>
                                 <IconFile className="ltr:mr-2 rtl:ml-2" />
@@ -134,7 +106,49 @@ const ReportTableLayout: React.FC<ReportTableLayoutProps> = ({ title, data, tota
                     )} */}
                 </div>
             </div>
-
+            <div className="mt-5">
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
+                    <div className="mb-5">
+                        <div className="dropdown">
+                            <label htmlFor="leadStatus">Select User</label>
+                            <select className="form-input">
+                                <option value="">[-Select-]</option>
+                                <option value="hot">Hot</option>
+                                <option value="cold">Cold</option>
+                                <option value="warm">Warm</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="mb-5">
+                        <div className="dropdown">
+                            <label htmlFor="leadStatus">Select Center </label>
+                            <select className="form-input">
+                                <option value="">[-Select-]</option>
+                                <option value="hot">Hot</option>
+                                <option value="cold">Cold</option>
+                                <option value="warm">Warm</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 ">
+                    <div className="mb-5">
+                        <ComponentsFormDatePickerRange />
+                    </div>
+                    <div className="flex items-center">
+                        <div>
+                            <button type="button" className="btn btn-primary mr-5">
+                                Submit
+                            </button>
+                        </div>
+                        <div>
+                            <button type="button" className="btn btn-outline-primary mr-5" onClick={handleExport}>
+                                Export to Excel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="panel mt-5 overflow-hidden border-0 p-0">
                 <div className="table-responsive">{/* <PaginationTable data={filterItem} tableColumns={tableColumns} handleDelete={handleDelete} handleEdit={handleEdit} /> */}</div>
             </div>
--- components/lead-management/lead-manage/components-form-date-picker-range.tsx
+++ components/lead-management/lead-manage/components-form-date-picker-range.tsx
@@ -20,7 +20,7 @@ const ComponentsFormDatePickerRange = () => {
                     position: isRtl ? 'auto right' : 'auto left',
                 }}
                 defaultValue={date3}
-                className="form-input"
+                className="form-input w-full"
                 onChange={(date3) => setDate3(date3)}
             />
         </div>
