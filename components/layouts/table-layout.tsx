922752f3252e3c987226664753341ab92eec1b55
Rajeswari21300 <rajeswari21300@gmail.com>
2024-07-15 13:57:25 +0530

    next followup modal & table in lead modal

--- components/layouts/table-layout.tsx
+++ components/layouts/table-layout.tsx
@@ -44,7 +44,8 @@ const TableLayout: React.FC<TableLayoutProps> = ({ title, filterby, data, setDat
     const [assignPasswordValue, setAssignPasswordValue] = useState<any>();
     const [assignPassword, setAssignPassword] = useState<boolean>(false);
     const [showCustomizer, setShowCustomizer] = useState(false);
-
+    const [followUps, setFollowUps] = useState([]);
+   
     const router = useRouter();
 
     useEffect(() => {
@@ -66,7 +67,7 @@ const TableLayout: React.FC<TableLayoutProps> = ({ title, filterby, data, setDat
             // const url = `/manage-visa?data=${data}`;
             // router.push(url);
             // debugger;
-            console.log(object);
+            // console.log(object);
             // sessionStorage.setItem('iseditmode', 'true');
             // sessionStorage.setItem('manageVisaData', JSON.stringify(object));
             router.push(`/manage-visa`);
@@ -190,6 +191,8 @@ const TableLayout: React.FC<TableLayoutProps> = ({ title, filterby, data, setDat
                 addData={addData}
                 isEdit={isEdit}
                 setIsEdit={setIsEdit}
+                followUps={followUps}
+                setFollowUps={setFollowUps}
             />
 
             <ReuseActionModal isOpen={isOpenAddNote} setIsOpen={setIsOpenAddNote} width="">
--- components/lead-management/lead-manage/components-form-date-picker-time.tsx
+++ components/lead-management/lead-manage/components-form-date-picker-time.tsx
@@ -13,60 +13,51 @@ interface ComponentsFormDatePickerBasicProps {
     isEdit?: boolean;
 }
 
-const formatDate = (date: Date) => {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-    const year = date.getFullYear();
+const formatTime = (date: Date) => {
     let hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    const strTime = `${hours}:${minutes}:${seconds} ${ampm}`;
-    return `${day}/${month}/${year} ${strTime}`;
+    return `${hours}:${minutes} ${ampm}`;
 };
 
 const ComponentsFormDatePickerTime: React.FC<ComponentsFormDatePickerBasicProps> = ({ id, setAddData, addData, isEdit }) => {
     const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl';
-    const time = new Date().toLocaleTimeString();
-    const [date4, setDate4] = useState<any>();
-    console.log('date', date4, addData);
+    const [time, setTime] = useState<string | undefined>(undefined);
+    const [date4, setDate4] = useState<Date[]>([]);
+
     useEffect(() => {
         if (isEdit && addData && id && addData[id]) {
-            setDate4(addData[id]);
+            setTime(addData[id]);
         }
     }, [isEdit, addData, id]);
 
-
     const handleDateChange = (selectedDates: Date[]) => {
         const selectedDate = selectedDates[0];
-        const formattedDate = formatDate(selectedDate);
-        console.log('select', selectedDate, formattedDate);
-        setDate4(formattedDate);
+        const formattedTime = formatTime(selectedDate);
+        setTime(formattedTime);
         setAddData((prevData: any) => ({
             ...prevData,
-            [id]: formattedDate,
+            [id]: formattedTime,
         }));
     };
 
     return (
-        <>
-            <div className="mb-5">
-                <label htmlFor="date">Time</label>
-                <Flatpickr
-                    options={{
-                        noCalendar: true,
-                        enableTime: true,
-                        dateFormat: 'h:i K',
-                        position: isRtl ? 'auto right' : 'auto left',
-                    }}
-                    value={date4}
-                    className="form-input"
-                    onChange={handleDateChange}
-                />
-            </div>
-        </>
+        <div className="mb-5">
+            <label htmlFor="date">Time</label>
+            <Flatpickr
+                options={{
+                    noCalendar: true,
+                    enableTime: true,
+                    dateFormat: 'h:i K',
+                    position: isRtl ? 'auto right' : 'auto left',
+                }}
+                value={time}
+                className="form-input"
+                onChange={handleDateChange}
+            />
+        </div>
     );
 };
 
