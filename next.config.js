d4edffc3399910eed86756672f238014fa52e723
sanjay-3129 <ksanjay00197@gmail.com>
2024-08-05 15:17:05 +0530

    webpack-error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -7,6 +9,14 @@ const nextConfig = {
     },
     env: {
         API_BASE_URL: process.env.API_BASE_URL,
+    },
+    webpack: (config, { isServer }) => {
+        // Resolve the `@` alias to the project's root directory
+        config.resolve.alias['@'] = path.resolve(__dirname);
+
+        // Additional custom Webpack configuration can go here
+
+        return config;
     },
 };
 
