cbcc0c5f24dc2b1d47c64d3382d027827f76fb80
Rajeswari21300 <rajeswari21300@gmail.com>
2025-04-02 21:52:01 +0530

    conflit

--- components/auth/components-auth-login-form.tsx
+++ components/auth/components-auth-login-form.tsx
@@ -91,7 +91,8 @@ const ComponentsAuthLoginForm = ({ type }: { type: string }) => {
                 </div> */}
                 <button
                     type="submit"
-                    disabled={false}
+                    // disabled={true} // for pre-prod login button should be enabled, for prod it should be disabled
+
                     className="btn !mt-6 w-full border-0 bg-gradient-to-r from-[#005fbe] to-[#2eb9ff] uppercase text-white shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
                 >
                     Sign in
