4086088e17afdb9cbdb6f4a48cd9759fa299ae57
sanjay-3129 <ksanjay00197@gmail.com>
2025-04-03 23:38:19 +0530

    Icon Update

--- components/home/Attendence.tsx
+++ components/home/Attendence.tsx
@@ -1,17 +1,14 @@
-import IconFile from '../icon/icon-file';
-import IconLogout from '../icon/icon-logout';
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { TextSearch, X } from 'lucide-react';
+import IconLogout from '../icon/icon-logout';
 import { showMessage } from '@/utils/notification';
 import { stateCityData } from '@/utils/constant';
 import { useGetCountryVisaTypesQuery } from '@/services/api/cms/countryVisaTypeSlice';
 import { VisaType } from '@/entities/visa-type.entity';
 import CountrySearchDropdown from '../Reusable/country-selector/CountrySearchDropdown';
-import { useRouter } from 'next/navigation';
-import { useDispatch } from 'react-redux';
 import { setIsLoading } from '@/store/app.store';
-import IconRefresh from '../icon/icon-refresh';
-import IconTrash from '../icon/icon-trash';
-import IconHome from '../icon/icon-home';
 
 const Attendence = () => {
     const router = useRouter();
@@ -179,11 +176,11 @@ const Attendence = () => {
                 </div>
                 <div className=" m-auto  flex w-1/3 items-center justify-center">
                     <button type="button" className=" btn btn-primary w-2/4" onClick={handleSubmitAttendence}>
-                        <IconLogout className="ltr:mr-2 rtl:ml-2" />
+                        <TextSearch className="ltr:mr-2 rtl:ml-2" />
                         Check Requirements
                     </button>
                     <button type="button" className=" btn btn-primary ml-2 w-1/3" onClick={handleClear}>
-                        <IconHome className="ltr:mr-2 rtl:ml-2" />
+                        <X className="size-5 ltr:mr-2 rtl:ml-2" />
                         Clear
                     </button>
                 </div>
